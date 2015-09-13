8d7523ddd49a74d38b6aacf899c3731a62ad18b6
Sean Lee <freshdried@gmail.com>
2015-09-13 13:30:24 -0400

    updated name

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     output: {
         filename: "./[name]/bundle.js",
-        library: "reach",
+        library: "react-illiterate",
         libraryTarget: "umd"
     },
     module: {
