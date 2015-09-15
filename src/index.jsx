b77961990c07da8d2dd6841581c36bb0c8721584
Sean Lee <freshdried@gmail.com>
2015-09-14 21:08:52 -0400

    gonna have to require with babel system, no more amd!!

--- src/index.jsx
+++ src/index.jsx
@@ -1,44 +1,46 @@
-import React, {PropTypes, unmountComponentAtNode, findDOMNode} from "react";
+import React, {PropTypes, unmountComponentAtNode} from "react";
 
 export default React.createClass({
-    propTypes: {
-        src: PropTypes.string.isRequired,
-        classes: PropTypes.object.isRequired
-    },
-    componentWillMount() {
-        this.mountedNodes = [];
-    },
-    componentDidMount() {
-        let node = this.getDOMNode();
-        let shadowRoot = node.createShadowRoot();
-        shadowRoot.innerHTML = this.props.src;
+  propTypes: {
+    src: PropTypes.string.isRequired,
+    classes: PropTypes.object.isRequired
+  },
+  componentWillMount() {
+    this.mountedNodes = [];
+  },
+  componentDidMount() {
+    let node = this.getDOMNode();
+    let shadowRoot = node.createShadowRoot();
+    shadowRoot.innerHTML = this.props.src;
 
-        let classes = this.props.classes;
-        let keys = Object.keys(classes);
+    let classes = this.props.classes;
+    let keys = Object.keys(classes);
 
-        for (let i = 0; i < keys.length; i++) {
-            let key = keys[i];
-            let node = shadowRoot.getElementById(key);
-            let element = React.createElement(classes[key]);
-            React.render(element, node);
-            this.mountedNodes.push(node);
-        }
-    },
-    componentWillUnmount() {
-        for (let i = 0; i < this.mountedNodes.length; i++) {
-            unmountComponentAtNode(this.mountedNodes[i]);
-        }
-    },
-    render() {
-        return (
-            <div/>
-        );
+
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i];
+      let node = shadowRoot.getElementById(key);
+      console.log(key, node);
+      if (node === null) continue;
+
+      let element = React.createElement(classes[key]);
+      React.render(element, node);
+      this.mountedNodes.push(node);
     }
+  },
+  componentWillUnmount() {
+    for (let i = 0; i < this.mountedNodes.length; i++) {
+      unmountComponentAtNode(this.mountedNodes[i]);
+    }
+  },
+  render() {
+    return (
+        <div/>
+    );
+  }
 });
 
 
 //TODO:
-// - handle case if node is not found
 // - handle updating
-// - handle unmounting
 // - Think about how to handle styling
