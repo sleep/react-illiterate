717bc241097fac760a5891cc81257cc6e891fff7
Sean Lee <freshdried@gmail.com>
2015-09-16 18:07:11 -0400

    updated to be Hot Reloadable

--- src/index.jsx
+++ src/index.jsx
@@ -6,32 +6,69 @@ export default React.createClass({
     classes: PropTypes.object.isRequired
   },
   componentWillMount() {
-    this.mountedNodes = [];
+    this.elements = {}; //Memoize the elements in an map.
   },
-  componentDidMount() {
-    let node = this.getDOMNode();
-    let shadowRoot = node.createShadowRoot();
+
+  // This should be a idempotent, as React.render(element, node) is idempotent
+  __mountNodes() {
+    //invariant: nodes are not mounted
+    let shadowRoot = this.shadowRoot;
+    let classes = this.props.classes;
+
+
+    //set innerHTML
     shadowRoot.innerHTML = this.props.src;
 
-    let classes = this.props.classes;
-    let keys = Object.keys(classes);
+    let keys = Object.keys(classes)
+          .filter((key) => shadowRoot.getElementById(key));
 
+    //invariant: shadowRoot.getElementById(key) exists for all keys
 
-    for (let i = 0; i < keys.length; i++) {
-      let key = keys[i];
-      let node = shadowRoot.getElementById(key);
-      console.log(key, node);
-      if (node === null) continue;
+    //make elements
+    this.elements = keys.reduce((sum, key) => {
+      return Object.assign(sum, {
+        [key]: this.elements[key] || React.createElement(classes[key])
+      });
+    }, {});
 
-      let element = React.createElement(classes[key]);
-      React.render(element, node);
-      this.mountedNodes.push(node);
-    }
+    //render elements;
+    keys.forEach((key) => {
+      React.render(this.elements[key], shadowRoot.getElementById(key));
+    });
   },
+
+
+  __unmountNodes(nodes) {
+    nodes.forEach((node) => {unmountComponentAtNode(node);});
+  },
+
+  componentDidMount() {
+    let node = this.getDOMNode();
+    let shadowRoot = this.shadowRoot = node.createShadowRoot();
+
+    this.__mountNodes();
+  },
+
+  componentDidUpdate(prevProps) {
+    //unmount existing nodes
+    let toUnmount = Object.keys(prevProps.classes)
+          .map((key) => this.shadowRoot.getElementById(key))
+          .filter((node) => node);
+    this.__unmountNodes(toUnmount);
+
+    //(re-)mount
+    this.__mountNodes();
+  },
+
   componentWillUnmount() {
-    for (let i = 0; i < this.mountedNodes.length; i++) {
-      unmountComponentAtNode(this.mountedNodes[i]);
-    }
+    let nodes = Object.keys(this.props.classes)
+          .map((key) => this.shadowRoot.getElementById(key))
+          .filter((node) => node);
+
+    this.__unmountNodes(nodes);
+  },
+  shouldComponentUpdate(nextProps) {
+    return true;
   },
   render() {
     return (
